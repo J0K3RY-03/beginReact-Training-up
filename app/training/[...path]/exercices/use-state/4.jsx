02234c65784fa812827a782e931fc2a0a817f76e
J0K3RY-03 <jokery03@hotmail.com>
2024-05-07 23:54:10 +0200

    finished exo 4.1 4.2 useState

--- app/training/[...path]/exercices/use-state/4.jsx
+++ app/training/[...path]/exercices/use-state/4.jsx
@@ -3,7 +3,8 @@
 
 import clsx from "clsx";
 
-const Button = ({ children, variant, id }) => {
+const Button = ({ children, variant, id, onClick }) => {
+
   const buttonClass = clsx("btn ring-offset-2 ring-offset-base-100", {
     "btn-primary": variant === "primary",
     "btn-error": variant === "error",
@@ -12,33 +13,38 @@ const Button = ({ children, variant, id }) => {
   });
 
   return (
-    <button className={buttonClass} id={id}>
+    <button className={buttonClass} id={id} onClick={() => onClick(id)}>
       {children}
     </button>
   );
 };
 
 export default function App() {
+  const handleClick = (id) => alert(`You cliqued on the ${id}`);
+
   return (
     <div
-      // 🦁 Rajoute un `onClick` avec un arrow function
-      // 💡 (e) => {...}
-      // Récupère l'id de l'élément cliqué avec `target`
-      // Si `target` === `currentTarget` = container
-      // Sinon = id de l'élément cliqué
-      // Affiche une `alert` avec `alert`
+        onClick={(e) => {
+          const target = e.target;
+
+          if (target === e.currentTarget) {
+            alert(`You clicked on the container`);
+          }
+        }}
       className="flex flex-wrap gap-4 p-4"
     >
-      <Button variant={"primary"} id="eat-me">
+      <Button variant={"primary"}
+              id="eat-me"
+              onClick={(id) => handleClick(id)}>
         Eat me
       </Button>
-      <Button variant={"error"} id="love-me">
+      <Button variant={"error"} id="love-me" onClick={(id) => handleClick(id)}>
         Love me
       </Button>
-      <Button variant={"success"} id="drink-me">
+      <Button variant={"success"} id="drink-me" onClick={(id) => handleClick(id)}>
         Drink me
       </Button>
-      <Button variant={"warning"} id="leave-me">
+      <Button variant={"warning"} id="leave-me" onClick={(id) => handleClick(id)}>
         Eat me
       </Button>
     </div>
