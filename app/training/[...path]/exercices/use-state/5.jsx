09cb3e096cb3709cf26a5eb95c6f254df1771378
J0K3RY-03 <jokery03@hotmail.com>
2024-05-08 00:59:21 +0200

    finished exo useState 5.1 5.2 5.3

--- app/training/[...path]/exercices/use-state/5.jsx
+++ app/training/[...path]/exercices/use-state/5.jsx
@@ -1,29 +1,29 @@
 "use client";
 
 import { User2 } from "lucide-react";
+import {useState} from "react";
 
-function NameForm() {
-  // 🦁 Ajoute un `useState` pour le nom
-  // 💡 useState("")
+function NameForm({initialName}) {
+    const [name, setName] = useState(initialName);
+
+    const handleButtonClick = () => {
+        setName(initialName);
+    }
 
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="card w-full max-w-96 bg-neutral text-neutral-content">
         <div className="card-body items-center text-center">
           <h2 className="card-title">Name :</h2>
-          {/* 🦁 Affiche le `name` juste ici */}
-          {/* 🦁 Utilise un opérateur ternaire pour soit afficher "No name" en rouge */}
-          {/* 🦁 Soit afficher le `name` */}
-          <p className="text-error">No name</p>
+            {name ? <p>{name}</p> : <p className="text-error">No name</p>}
+            {initialName !== name ?
+                <button className={'btn btn-warning btn-sm'} onClick={handleButtonClick}>Reset</button> : null}
         </div>
       </div>
       <div className="divider">Form</div>
       <label className="input input-bordered flex items-center gap-2">
         <User2 size={16} />
-        {/* 🦁 Transforme cette input en **controlled input** */}
-        {/* Il faut ajouter la propriété `value` et `onChange` */}
-        {/* 💡 onChange={e => e.target.value} : pour récupérer la value */}
-        <input type="text" className="grow" placeholder="Melvynx" />
+        <input type="text" className="grow" placeholder="Melvynx" value={name} onChange={(e) => setName(e.target.value)} />
       </label>
     </div>
   );
@@ -32,7 +32,7 @@ function NameForm() {
 export default function App() {
   return (
     <div>
-      <NameForm />
+      <NameForm initialName={"Jason"}/>
     </div>
   );
 }
